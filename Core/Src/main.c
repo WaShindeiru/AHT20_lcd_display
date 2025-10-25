5777b63fe5430ee49f816af8324948bb41821c3b
WaShindeiru <franekdata@gmail.com>
2025-10-25 22:06:47 +0200

    initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,335 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+#include "lcd_i2c.h"
+#include <stdbool.h>
+
+#include "string.h"
+#include "math.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define LCD_ADDRESS (0x3F << 1)
+#define AHT_ADDRESS 0x38<<1
+#define AHT_STATUS_CODE 0x71
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+struct lcd_disp disp;
+
+float humidity;
+float temperature;
+
+uint8_t buffer[40];
+uint8_t aht_buffer[7] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+SemaphoreHandle_t i2c_mutex;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+/* USER CODE BEGIN PFP */
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+HAL_StatusTypeDef AHT20_Get_Status(uint8_t* statusWord) {
+	return HAL_I2C_Mem_Read(&hi2c1, AHT_ADDRESS, AHT_STATUS_CODE, 1, statusWord, 1, 1000);
+}
+
+HAL_StatusTypeDef AHT20_Initialize() {
+	uint8_t commands[3] = {0xbe, 0x08, 0x00};
+	HAL_StatusTypeDef returnValue;
+
+	returnValue = HAL_I2C_Master_Transmit(&hi2c1, AHT_ADDRESS, commands, 3, 1000);
+	vTaskDelay(pdMS_TO_TICKS(10));
+
+	return returnValue;
+}
+
+HAL_StatusTypeDef AHT20_Make_Measurement() {
+	uint8_t commands[3] = {0xAC, 0x33, 0x00};
+	HAL_StatusTypeDef returnValue;
+
+	returnValue = HAL_I2C_Master_Transmit(&hi2c1, AHT_ADDRESS, commands, 3, 1000);
+	if (returnValue != HAL_OK) {
+		return returnValue;
+	}
+
+	vTaskDelay(pdMS_TO_TICKS(80));
+
+	uint8_t statusWord = 0x80;
+
+	while ((statusWord>>7 & 0x01) == 1) {
+		AHT20_Get_Status(&statusWord);
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+
+	HAL_I2C_Master_Receive(&hi2c1, AHT_ADDRESS, aht_buffer, 7, 1000);
+
+	uint32_t hum_data = (aht_buffer[1]<<16)|(aht_buffer[2]<<8)|aht_buffer[3];
+	hum_data = hum_data>>4;
+	humidity = (float) ((hum_data / pow(2, 20)) * 100);
+
+	uint32_t temp_data = (aht_buffer[3]<<16)|(aht_buffer[4]<<8)|aht_buffer[5];
+	temp_data = temp_data & 0xFFFFF;
+	temperature = (float) (((temp_data / pow(2, 20)) * 200) - 50);
+
+	return returnValue;
+}
+
+void LCDTask(void* pvParameters) {
+	disp.addr = LCD_ADDRESS;
+	disp.bl = true;
+	lcd_init(&disp);
+	BaseType_t status;
+
+	for (;;) {
+		sprintf((char *)disp.f_line, "Temp: %.2f", temperature);
+		sprintf((char *)disp.s_line, "Humd: %.2f", humidity);
+
+		status = xSemaphoreTake( i2c_mutex, pdMS_TO_TICKS(2000) );
+		if (status == pdTRUE)
+		{
+			lcd_display(&disp);
+			xSemaphoreGive( i2c_mutex );
+			vTaskDelay(pdMS_TO_TICKS(2000));
+		} else {
+			vTaskDelay(pdMS_TO_TICKS(40));
+		}
+	}
+}
+
+void AHT20Task(void* pvParameters) {
+	vTaskDelay(pdMS_TO_TICKS(40));
+	AHT20_Initialize();
+	BaseType_t status;
+
+	for (;;) {
+		status = xSemaphoreTake( i2c_mutex, pdMS_TO_TICKS(2000) );
+		if (status == pdTRUE)
+		{
+			AHT20_Make_Measurement();
+			xSemaphoreGive( i2c_mutex );
+			vTaskDelay(pdMS_TO_TICKS(1000));
+		} else {
+			vTaskDelay(pdMS_TO_TICKS(40));
+		}
+	}
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+  i2c_mutex = xSemaphoreCreateMutex();
+  configASSERT(i2c_mutex != NULL);
+
+  BaseType_t status;
+
+  status = xTaskCreate(LCDTask, "LCDTask", 200, NULL, 4, NULL);
+  configASSERT(status == pdPASS);
+
+  status = xTaskCreate(AHT20Task, "AHT20Task", 200, NULL, 4, NULL);
+  configASSERT(status == pdPASS);
+
+
+  vTaskStartScheduler();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
